8ca2d54e8275ab6ea71828621f8e86001375d190
Youssef <youssef.sharief@thoughtdesign.com.au>
2017-11-14 13:03:46 +0200

    Update cyclicRotation

--- arrays/cyclicRotation.js
+++ arrays/cyclicRotation.js
@@ -1,4 +1,14 @@
-function solution(arr,k){
-    return arr.concat(arr.splice(0,arr.length-k));
+function solution(A, K) {
+    // edge cases
+    if (K === 0) return A;
+    if (A.length === 1) return A
+    if (K > A.length) K = K % A.length;
+    const seperationIndex = A.length - K;
+    const lastPortion = A.slice(-K);
+    const firstPortion = A.slice(0, seperationIndex);
+    return lastPortion.concat(firstPortion);
 }
 
+const A = [3, 8, 9, 7, 6];
+let t = solution(A, 8)
+t
